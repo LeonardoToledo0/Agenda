e405072e9d88148c6c01e9228f7bbbbf699d4254
Leonardo Toledo <leotoledo010@gmail.com>
2023-11-06 21:24:35 -0300

    Incluir area de interesse

--- AgendaApp/admin.py
+++ AgendaApp/admin.py
@@ -2,19 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 
-from AgendaApp.models import Contato,Cidade,Telefone
-
+from AgendaApp.models import Contato,Cidade,Telefone,Interesse
 
 
 class Telefones(admin.StackedInline):
     model = Telefone
+    extra = 1
 
 class ContatoAdmin(admin.ModelAdmin):
     
-    list_display = ['id','nome','apelido','data_nascimento',"estado_civil",'estado','cidade']
-    list_filter = ['id','nome','data_nascimento','cidade', 'estado']
+    list_display = ['id','nome','apelido','data_nascimento','estado_civil','estado','cidade']
+    list_display_links = ['id','nome']
+    list_filter = ['id','nome','data_nascimento','cidade', 'estado',]
     search_fields= ['id','nome', 'apelido']
     inlines = [Telefones]
+    filter_horizontal =['interesses']
 
 class TelefoneAdmin(admin.ModelAdmin):
     list_display = ['ddd','numero','tipo','IsWatsApp']
@@ -24,3 +26,4 @@ class TelefoneAdmin(admin.ModelAdmin):
 admin.site.register(Contato,ContatoAdmin)
 admin.site.register(Cidade)
 admin.site.register(Telefone,TelefoneAdmin)
+admin.site.register(Interesse)
